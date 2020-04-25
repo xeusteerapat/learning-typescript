46a448b026b4b3caf98936cf34468dcb49421b92
xeusteerapat <xeus085@gmail.com>
2020-04-25 14:01:19 +0700

    add decorators subject

--- 06-csv-stats/src/index.ts
+++ 06-csv-stats/src/index.ts
@@ -1,23 +1,12 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './MatchResult';
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8'
-  })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
-
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D'
-}
+const reader = new CsvFileReader('football.csv');
+reader.read();
 
 let arsenalWins = 0;
 
-for (let match of matches) {
+for (let match of reader.data) {
   if (match[1] === 'Arsenal' && match[5] === MatchResult.HomeWin) {
     arsenalWins++;
   } else if (match[2] === 'Arsenal' && match[5] === MatchResult.AwayWin) {
